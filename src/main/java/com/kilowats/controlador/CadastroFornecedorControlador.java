a37864ae71ee335c05ac1e47c75e4c1bb90f1f5c
ErikQueiroz <ErikQueiroz@24c8bfdd-ee60-40b8-b23f-936616d00f54>
2016-03-18 20:57:51 +0000

    Ajustes no Converter.

--- src/main/java/com/kilowats/controlador/CadastroFornecedorControlador.java
+++ src/main/java/com/kilowats/controlador/CadastroFornecedorControlador.java
@@ -9,20 +9,72 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
+import com.kilowats.entidades.Cidade;
 import com.kilowats.entidades.Empresa;
+import com.kilowats.entidades.Endereco;
 import com.kilowats.entidades.Telefone;
 import com.kilowats.enuns.TipoTelefoneEnum;
+import com.kilowats.interfaces.IValidacaoCadastro;
 import com.kilowats.utils.Utils;
+import com.kilowats.validadores.ValidacaoCadastroEndereco;
+import com.kilowats.validadores.ValidacaoCadastroFornecedor;
 
 @ManagedBean
 @ViewScoped
 public class CadastroFornecedorControlador implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
-	private Empresa empresa = new Empresa(); 
+	private Empresa empresa = new Empresa();
+	private Endereco endereco = new Endereco();
+	private Telefone telefone = new Telefone();
+	private Cidade cidade = new Cidade();
 	
 	public String carregaMascaraCnpjOuCpfPrimefaces(){
-		return Utils.mascarPrimefacesCnpjOuCpf(empresa.getFisicaJuridica());
+		int tipo = 0;
+		if(empresa != null && empresa.getFisicaJuridica() != null){
+			tipo = empresa.getFisicaJuridica();
+		}
+		return Utils.mascarPrimefacesCnpjOuCpf(tipo);
 	}
+	
+	public void salvar(){
+		FacesContext context = FacesContext.getCurrentInstance();
+		IValidacaoCadastro validacao = new ValidacaoCadastroEndereco();
+		this.endereco.setCidade(this.cidade);
+		if(validacao.validarCadastro(endereco)){
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Cadastro Fornecedor Realizado com sucesso!", null));
+		}else{
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Não foi possível realizar o cadastro do Fornecedor!", null));
+		}
+	}
+
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	public Telefone getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(Telefone telefone) {
+		this.telefone = telefone;
+	}
+	public Cidade getCidade() {
+		return cidade;
+	}
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+	
+	
 
 }
