42738dfa89fd263bd229a1dd068678e16f816e5e
Erik5594 <erik_derick@hotmail.com>
2016-07-28 11:37:33 -0300

    Create AppUserDetailsService.java

---
+++ src/main/java/com/kilowats/security/AppUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.kilowats.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.kilowats.dao.UsuarioDao;
+import com.kilowats.entidades.Grupo;
+import com.kilowats.entidades.Usuario;
+
+public class AppUserDetailsService implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		UsuarioDao usuarioD = new UsuarioDao();
+		Usuario usuario = usuarioD.porEmail(email);
+		UsuarioSistema user = null;
+		if(usuario != null){
+			user = new UsuarioSistema(usuario, getGrupos(usuario));
+		}
+		return user;
+	}
+
+	private Collection<? extends GrantedAuthority> getGrupos(Usuario usuario) {
+		List<SimpleGrantedAuthority> grupos = new ArrayList<>();
+		
+		for(Grupo grupo : usuario.getGrupos()){
+			grupos.add(new SimpleGrantedAuthority(grupo.getNome().toUpperCase()));
+		}
+		
+		return grupos;
+	}
+
+}
