6525ec2530a872e50285289573807d5f45178fed
Erik <erik_derick@hotmail.com>
2017-01-19 17:16:51 -0200

    Add files via upload

---
+++ src/main/java/com/github/erik5594/interfaces/GerarPDF.java
@@ -0,0 +1,9 @@
+package com.github.erik5594.interfaces;
+
+import java.io.IOException;
+
+import net.sf.jasperreports.engine.JRException;
+
+public interface GerarPDF {
+	public void gerarPDF() throws JRException, IOException;
+}
---
+++ src/main/java/com/github/erik5594/interfaces/IValidacaoCadastro.java
@@ -0,0 +1,5 @@
+package com.github.erik5594.interfaces;
+
+public interface IValidacaoCadastro {
+	boolean validarCadastroComMensagem(Object obj, String titulo, boolean mostrarMensagem);
+}
